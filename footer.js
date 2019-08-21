e49abfcab4c19c8098fc01a1b8fd5cacc5e7d0c3
Aeon <aeon.williams@digipen.edu>
2019-08-20 17:22:55 -0700

    Footer & Help
    Footer & help button were implemented.

---
+++ footer.js
@@ -0,0 +1,6 @@
+document.write(`\
+<br><br><br><br><br><br><br>\
+<div class="footer" style="font-size: x-small">\
+Last updated: 08/08/19 | Consistent with version 2.0.6 | © 2019 DigiPen, All Rights Reserved.\
+</div>\
+ ` );
