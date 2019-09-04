297ce3de4190d1949a58d786be87592519151c72
Unknown <aeon.williams@digipen.edu>
2019-09-03 17:58:39 -0700

    Project 5 Uploaded
    Project 5 is fully uploaded and added to the exmaple projects.

--- top_bar.js
+++ top_bar.js
@@ -1,22 +1,21 @@
 document.write(`\
-<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">\
-    <a class="navbar-brand" href="index.html">\
-        <img src="lpk_logo_smol.png" width="60" height="60"></img>\
-        DragonPack\
-    </a>\
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">\
-              <span class="navbar-toggler-icon"></span>\
-            </button>\
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">\
-        <div class="navbar-nav">\
-            <a class="nav-item nav-link" href="download.html">Download</a>\
-            <a class="nav-item nav-link" href="example_projects.html">Documentation</a>\
-        </div>\
-    </div>\
-    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">\
-        <ul class="navbar-nav ml-auto">\
-            <li><a class="far fa-question-circle" href="help.html" style="color:#DDDDDD" target="_blank"></a></li>\
-        </ul>\
-    </div>\
-</nav>\
- ` );
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">\
+    <a class="navbar-brand" href="index.html">\
+        <img src="lpk_logo_smol.png" width="60" height="60"></img>\
+        DragonPack\
+    </a>\
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">\
+              <span class="navbar-toggler-icon"></span>\
+            </button>\
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">\
+        <div class="navbar-nav">\
+            <a class="nav-item nav-link" href="download.html">Download</a>\
+            <a class="nav-item nav-link" href="example_projects.html">Documentation</a>\
+        </div>\
+    </div>\
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">\
+        <ul class="navbar-nav ml-auto">\
+            <li><a class="far fa-question-circle" href="help.html" style="color:#DDDDDD" target="_blank"></a></li>\
+        </ul>\
+    </div>\
+</nav>\ ` );
